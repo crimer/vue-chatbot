360986eac069f2a082357cb732ca8ce2ce1c66ad
Mark Vodyanitskiy <mvodya@gmail.com>
2021-01-09 23:50:34 +1000

    Добавил Sentry

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import Vue from "vue";
+
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
+
 import App from "./App.vue";
 import store from "./store/index";
 import timeFilter from "./filter/time.filter";
+
+Sentry.init({
+  Vue,
+  dsn: "https://fe4f7257d085491caab0e7d946a67d5c@dsn.frogling.com/5",
+  autoSessionTracking: true,
+  logErrors: true,
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
+
 // подключение аильтра dateFilter по имени dateFilter
 Vue.filter('timeFilter',timeFilter);
 
