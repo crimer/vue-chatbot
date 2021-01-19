fa8c6559786bede50d1670e8545cd72bdf7f90fb
TheBandik <shnr.arky@gmail.com>
2021-01-19 19:32:35 +1000

    Получение номера телефона и сессии

--- src/components/ModalPhone.vue
+++ src/components/ModalPhone.vue
@@ -3,9 +3,9 @@
   <form class="modal__phone" :class="{ isActive: isModalPhoneOpen }">
   <h1 class="modal__phone__title">Остались вопросы?   Оставьте номер телефона и мы перезвоним вам</h1>
   <div class="modal__phone__number">
-    <input v-model="phone" class="modal__phone__input " placeholder="+79001235678">
+    <input v-model="phone" class="modal__phone__input" placeholder="+79001235678">
   </div>
-  <button class="modal__phone__button" @click="sendPhone(phone)">Отправить</button>
+  <button class="modal__phone__button" @click="sendPhone">Отправить</button>
   <button class="modal__phone__button">Отмена</button>
  </form>
  </div>
@@ -17,15 +17,20 @@ import { mapState, mapMutations, mapActions} from "vuex";
 export default {
   name: "ModalPhone",
   computed: {
-    ...mapState(["isModalPhoneOpen"])
+    ...mapState(["isModalPhoneOpen", "session"])
+  },
+  data() {
+    return {
+      phone: null
+    }
   },
   methods: {
-    ...mapActions(["SEND_PHONE"]),
-    sendPhone(phone) {
-      this.SEND_PHONE(phone);
+    sendPhone() {
+      fetch(`http://127.0.0.1:8000/api/chat/phone/?id=${this.session}&phone=${this.phone}`);
     }
-  }
+  },
 };
+
 </script>
 
 <style  lang="stylus">
